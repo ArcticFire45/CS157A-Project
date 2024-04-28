bdfb8d1c45714219dfe6ca2cb53c76428834a318
Kevin Rai <kevin.rai@sjsu.edu>
2024-04-27 23:53:45 -0700

    moving card into own component

--- pokemonfrontend/src/components/PokemonPage.js
+++ pokemonfrontend/src/components/PokemonPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import usePokemons from "../hooks/usePokemons";
+import Card from "./Card";
 
 const PokemonPage = () => {
   const pokemons = usePokemons();
@@ -12,23 +13,11 @@ const PokemonPage = () => {
         style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
       >
         {pokemons.map((pokemon) => (
-          <div
+          <Card
             key={pokemon.poke_id}
-            style={{
-              margin: "10px",
-              padding: "10px",
-              border: "1px solid #ccc",
-              borderRadius: "8px",
-              textAlign: "center",
-            }}
-          >
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.poke_id}.png`}
-              alt={pokemon.poke_name}
-              style={{ width: "100px", height: "100px" }}
-            />
-            <p style={{ marginTop: "5px" }}>{pokemon.poke_name}</p>
-          </div>
+            id={pokemon.poke_id}
+            name={pokemon.poke_name}
+          />
         ))}
       </div>
     </div>
