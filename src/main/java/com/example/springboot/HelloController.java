b5750543163ee2deac62fd1e8ccd8b407ab57918
chels-c <94879318+chels-c@users.noreply.github.com>
2024-04-30 03:08:31 +0000

    signup and login

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @CrossOrigin
@@ -30,5 +34,28 @@ public class HelloController {
 		return this.pokeservice.getPokemonName("1");
 	}
 
+	@PostMapping("/signup")
+	public ResponseEntity<?> signup(@RequestParam String user, @RequestParam String pass) {
+		try{
+			this.pokeservice.addUser(user, pass);
+			return ResponseEntity.ok("User registered.");
+		}catch(Exception e){
+			return ResponseEntity.badRequest().body("Registration failed: " + e.getMessage());
+		}
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> postMethodName(@RequestBody String user,  @RequestParam String pass) {
+		try {
+            boolean loggedIn = this.pokeservice.loginUser(user, pass);
+            if (loggedIn) {
+                return ResponseEntity.ok("User logged in.");
+            } else {
+                return ResponseEntity.status(401).body("Login failed: Invalid username or password");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Internal server error: " + e.getMessage());
+        }
+	}
 
 }
