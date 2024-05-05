7307d7ccceaf30bf759760f4a49868c46274a8cf
Lawton Fong <57239569+lawtonfong@users.noreply.github.com>
2024-05-05 01:28:44 -0700

    fixed userposts and userpostserviceimplementation

--- src/main/java/com/example/springboot/UserPostsServiceImplementation.java
+++ src/main/java/com/example/springboot/UserPostsServiceImplementation.java
@@ -11,44 +11,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostsServiceImplementation {
+public class UserPostsServiceImplementation {
 
     @Autowired
-    static List<Posts> PokemonPosts = new ArrayList<Posts>();
+    static List<UserPosts> userPostsList = new ArrayList<UserPosts>();
 
     Connection connection;
 
-    public PostsServiceImplementation() throws SQLException {
+    public UserPostsServiceImplementation() throws SQLException {
         connection = DBUtil.getConnection();
     }
 
-    public List<Posts> getAllPokemonPosts() {
+    public List<UserPosts> getAllUserPosts() {
         try {
-            PokemonPosts = new ArrayList<Posts>();
+            userPostsList = new ArrayList<>();
             PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Posts");
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
-                Posts post = new Posts(rs.getInt(1), rs.getString(2), rs.getString(3));
-                PokemonPosts.add(post);
+                UserPosts post = new UserPosts(rs.getInt("PostID"), rs.getString("description"), rs.getString("image_url"));
+                userPostsList.add(post);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-            return PokemonPosts;
+            return userPostsList;
 
         }
 
-    public void createPokemonPost(Posts post) {
+    public void createPokemonPost(UserPosts post) {
         try {
             PreparedStatement stmt = connection.prepareStatement("INSERT INTO pokemon_posts (post_content, author) VALUES (?, ?)");
-            stmt.setString(1, post.getPostContent());
-            stmt.setString(2, post.getAuthor());
+            stmt.setString(1, post.getDescription());
+            stmt.setString(2, post.getImage_url());
             int rs = stmt.executeUpdate();
 
             if (rs > 0) {
                 System.out.println("Pokemon post created successfully!");
             } else {
                 System.out.println("Failed to create pokemon post.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deletePokemonPost(Integer postId) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement("DELETE FROM pokemon_posts WHERE PostID = ?");
+            stmt.setInt(1, postId);
+            int rs = stmt.executeUpdate();
+
+            if (rs > 0) {
+                System.out.println("Pokemon post deleted successfully!");
+            } else {
+                System.out.println("Failed to delete pokemon post.");
             }
         } catch (SQLException e) {
             e.printStackTrace();
